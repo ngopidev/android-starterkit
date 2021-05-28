56d794a969847b925a04bfaf8434b3b7e3136407
Irfan <assidiq.irfan@gmail.com>
2021-05-28 08:19:38 +0700

    first init for starterkit

---
+++ app/src/main/java/com/ngopidev/project/starterkit/activities/LoginActivity.kt
@@ -0,0 +1,9 @@
+package com.ngopidev.project.starterkit.activities
+
+
+/**
+ *   created by Irfan Assidiq
+ *   email : assidiq.irfan@gmail.com
+ **/
+class LoginActivity {
+}
---
+++ app/src/main/java/com/ngopidev/project/starterkit/helper/MethodHelper.kt
@@ -0,0 +1,13 @@
+package com.ngopidev.project.starterkit.helper
+
+
+/**
+ *   created by Irfan Assidiq
+ *   email : assidiq.irfan@gmail.com
+ *   digunakan untuk menyimpan beberapa method yang membantu untuk mempermudah beberapa proses
+ *   yang ada didalam android, seperti mempersingkat pembuatan toast dan beberapa hal
+ *   yang kemungkinan akan digunakan didalam aplikasi android yang dibuat.
+ **/
+object MethodHelper {
+
+}
